f30f33507e3e61f32b327d92ea7509d35c4e405b
Artem Petrikov <manforten@tandex.ru>
2022-10-14 00:38:06 +0300

    new commit

--- server.py
+++ server.py
@@ -1,11 +1,28 @@
+import socket
 import threading
 
+ip = "localhost"
+port = 9090
+server = socket.socket()
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((ip, port))
+print("Включение сервера")
+print("Ожидание подключения пользователя..")
+while True:
+    server.listen(1)
+    conn, addr = server.accept()
 
-def proc(n):
-    print("Процесс", n)
+    print ("Новое подключение: ", addr)
 
 
-p1 = threading.Thread(target=proc, name="t1", args=["1"])
-p2 = threading.Thread(target=proc, name="t2", args=["2"])
-p1.start()
-p2.start()
+    print ("Подключение от: ", addr[0])
+    while True:
+        data = conn.recv(2048)
+        msg = data.decode()
+        if msg == 'exit':
+            break
+        print (f"Сообщение от {addr[0]}: {msg}")
+        conn.send(bytes(msg, 'UTF-8'))
+    print ("Клиент ", addr[0], " отключился...")
+
+
