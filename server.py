795fe3bae94ccad19616a227c7b73b081ca248f4
Artem Petrikov <manforten@tandex.ru>
2022-10-18 15:25:27 +0300

    New class

--- server.py
+++ server.py
@@ -1,28 +1,37 @@
 import socket
 import threading
 
+class ClientThreading(threading.Thread):
+    def __init__(self, addr, conn):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+        print("----------------------------")
+        print (f"Новое подключение от {self.addr}")
+        print("----------------------------")
+
+    def run(self):
+        print (f"Подключение от {self.addr}")
+        print("----------------------------")
+        while True:
+            data = self.conn.recv(2048)
+            msg = data.decode()
+            if msg == 'exit':
+                break
+            print (f"Сообщение от {self.addr}: {msg}")
+            self.conn.send(msg.encode())
+        print (f"Клиент {self.addr} отключился!")
+
 ip = "localhost"
-port = 9090
-server = socket.socket()
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((ip, port))
-print("Включение сервера")
-print("Ожидание подключения пользователя..")
+port = 9091
+TYPE = socket.AF_INET
+PROTOCOL = socket.SOCK_STREAM
+sock = socket.socket(TYPE,PROTOCOL)
+sock.bind((ip, port))
+print("Включение сервера!")
+print("Ожидание подключения клиента...")
 while True:
-    server.listen(1)
-    conn, addr = server.accept()
-
-    print ("Новое подключение: ", addr)
-
-
-    print ("Подключение от: ", addr[0])
-    while True:
-        data = conn.recv(2048)
-        msg = data.decode()
-        if msg == 'exit':
-            break
-        print (f"Сообщение от {addr[0]}: {msg}")
-        conn.send(bytes(msg, 'UTF-8'))
-    print ("Клиент ", addr[0], " отключился...")
-
-
+    sock.listen(5)
+    conn, addr = sock.accept()
+    thread = ClientThreading(addr[0], conn)
+    thread.start()
