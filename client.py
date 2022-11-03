95c92bef02d21d155f67286eba91bfb99263ee27
Artem Petrikov <manforten@tandex.ru>
2022-11-03 17:14:58 +0300

    New changes with input name client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 
 m = 0
+name = input('Введите ваше имя: ')
 F = input('Если вы хотите ввести номер порта и имя хоста, напишите да:')
 if F == 'да' or F == 'Да':
     host = input('Введите имя хоста:')
@@ -11,11 +12,12 @@ sock = socket.socket()
 sock.setblocking(1)
 
 if m == 0:
-    sock.connect(('localhost', 9101))
+    sock.connect(('localhost', 9131))
 else:
     sock.connect((host,port_number))
 
 while True:
+    print('Имя пользователя:',name)
     msg = input('Введите сообщение: ')
     sock.send(msg.encode())
     data = sock.recv(1024)
